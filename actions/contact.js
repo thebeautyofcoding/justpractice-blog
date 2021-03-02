9a140f4a94cb47facff371658bb3a163db899e0e
heiner <heiner.giehl@yahoo.com>
2021-03-02 22:44:51 +0100

    ...

--- actions/contact.js
+++ actions/contact.js
@@ -6,7 +6,7 @@ export const emailContactForm = async(data) => {
 
     let emailEndpoint;
     console.log(data)
-    if (data.authEmail) emailEndpoint = `${API}/contact-blog-author`
+    if (data.authorEmail) emailEndpoint = `${API}/contact-blog-author`
     else emailEndpoint = `${API}/contact`
     
   const response = await axios.post(emailEndpoint, 
