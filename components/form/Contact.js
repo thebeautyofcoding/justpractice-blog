7294b335ddb3995ddfee36a5f101f7317a5bb3b6
heiner <heiner.giehl@yahoo.com>
2021-03-03 00:03:44 +0100

    ..

--- components/form/Contact.js
+++ components/form/Contact.js
@@ -24,7 +24,10 @@ const Contact = ({authorEmail}) => {
             setValues({ ...values, buttonText: 'Sending...' })
             const data = await emailContactForm({authorEmail,name, email, message})
             console.log(data)
-            setValues({ ...values, buttonText: 'Sent', error: '', sent: true, name: '', email: '' })
+            setValues({ ...values, buttonText: 'Sent', error: '', sent: true, name: '', email: '', message:'' })
+            // setTimeout(() => {
+            //     setValues({...values, sent:false})
+            // }, 3000)
 
         } catch (err) {
             console.log(err.response.data)
@@ -35,11 +38,16 @@ const Contact = ({authorEmail}) => {
 
     const handleChange = (name) => e => {
         setValues({ ...values, [name]: e.target.value, error: false, success: false })
+        
     }
 
-    return <form onSubmit={onSubmitHandler} className="container__column mt-small">
+   const resetButtonTextAndSentStatus = () => {
+        if(sent) setValues({...values, sent:false, buttonText:'Another message?'})
+    }
+
+    return <form onKeyUp={resetButtonTextAndSentStatus} onSubmit={onSubmitHandler} className="container__column mt-small">
         <input type="text" onChange={handleChange('name')} className="input " value={name} name="name" placeholder="Please type your name..." required></input>
-        <input type="email" onChange={handleChange('email')} className="input mt-small" value={email} name="email" required placeholder="Please type your email..."></input>
+        <input type="email" onChange={handleChange('email')} className="input mt-small" value={email} name="email" required placeholder="Please type the email of the recipient..."></input>
         <textarea rows={10} type="text" onChange={handleChange('message')} required className="input mt-small" value={message} name="message" placeholder="Please type your message..."></textarea>
 
         <button className="Button mt-small" type="submit">{buttonText}</button>
